228800d507a1f95935ed14a23f108b29c9a59153
Clement Osuide <clementosuide@Clements-MacBook-Pro.local>
2023-06-14 00:20:17 +0100

    D

--- server/cron/googlesheet.js
+++ server/cron/googlesheet.js
@@ -1,7 +1,16 @@
 const connectDB = require("../config/db");
 const googlesheet = require("../helperfunctions/googlesheet");
+const delay = (ms)=>{
+  return new Promise((resolve,reject)=> {
+    setTimeout(()=>{
+      resolve(res)
+    }, ms)
+  })
+}
 (async () => {
+  await delay(5000)
   await connectDB();
+
   await googlesheet();
   process.exit(1);
 })();
